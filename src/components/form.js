48887d0125465983d8ea67e84e11469c37a00b51
Terri <thowze@skillstorm.com>
2021-10-21 22:03:17 -0400

    Form created

---
+++ src/components/form.js
@@ -0,0 +1,16 @@
+const loginform =() => {
+    return(
+        <form onSubmit={handleSubmit}>
+            <label>Enter your username:
+            <input 
+            type="text"
+            />
+            </label>
+            <label>Enter your
+
+            </label>
+        </form>
+    )
+}
+
+export default loginform
