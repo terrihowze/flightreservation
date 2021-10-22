c926e3763169f3df834b72492c00373361f0a2fe
Terri <thowze@skillstorm.com>
2021-10-22 15:14:44 -0400

    Added form to main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Header from './components/Header'
-
+import Post from './components/form'
 function App() {
   return (
     <div className='container'>
       <Header />
-      
+      <Form />
     </div>
   );
 }
